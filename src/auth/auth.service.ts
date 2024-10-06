056436e20fe3db7b73b97ef092e1364c2e9a7dfa
unknown <caiof.marques@yahoo.com.br>
2024-10-06 19:31:19 -0300

    Finished auth system.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,30 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from "bcrypt";
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './interfaces/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './interfaces/UserToken';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) { }
 
-    constructor(private readonly userService: UserService) { }
+    login(user: User): UserToken {
+        const payload: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            name: user.name,
+        }
+
+        const jwtToken = this.jwtService.sign(payload)
+        return {
+            access_token: jwtToken
+        }
+    }
 
     async validateUser(email: string, password: string) {
 
