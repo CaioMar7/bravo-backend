316867b6244e86e8cba126b9247756cb555a33c4
unknown <caiof.marques@yahoo.com.br>
2024-10-06 19:01:32 -0300

    Revert "Initial login config."
    This reverts commit cf183b6fd30004ad54d34037c9cd6888b29a4b35.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AthModule } from './ath/ath.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AthModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
